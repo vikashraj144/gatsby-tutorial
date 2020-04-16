5627536521e073627d5787ef11cf4339ba703100
vikash <vikashraj144@gmail.com>
2020-04-17 01:15:36 +0530

    gatsby node updated

--- authoring-themes-tutorial/gatsby-theme-events/gatsby-node.js
+++ authoring-themes-tutorial/gatsby-theme-events/gatsby-node.js
@@ -9,3 +9,38 @@ exports.onPreBootstrap = ({ reporter }) => {
     fs.mkdirSync(contentPath)
   }
 }
+
+// Define the "Event" type
+exports.sourceNodes = ({ actions }) => {
+  actions.createTypes(`
+    type Event implements Node @dontInfer {
+      id: ID!
+      name: String!
+      location: String!
+      startDate: Date! @dateformat @proxy(from: "start_date")
+      endDate: Date! @dateformat @proxy(from: "end_date")
+      url: String!
+      slug: String!
+    }
+  `)
+}
+
+// Define resolvers for custom fields
+exports.createResolvers = ({ createResolvers }) => {
+  const basePath = "/"
+  // Quick-and-dirty helper to convert strings into URL-friendly slugs.
+  const slugify = str => {
+    const slug = str
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/(^-|-$)+/g, "")
+    return `/${basePath}/${slug}`.replace(/\/\/+/g, "/")
+  }
+  createResolvers({
+    Event: {
+      slug: {
+        resolve: source => slugify(source.name),
+      },
+    },
+  })
+}
